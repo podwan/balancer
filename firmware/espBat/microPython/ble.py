9d97bed97909b5a8011eab2ee6dcd7aa66c575f5
jimmydwan <inffy2012@me.com>
2024-04-12 11:20:11 +0800

    update rfBat firmware

--- firmware/espBat/microPython/ble.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin
-from time import sleep_ms
-import ubluetooth
-
-ble = ubluetooth.BLE()
-ble.active(True)
-
-ble.gap_advertise(100, adv_data = b'\x02\x01\x06\x03\x09\x41\x42')
