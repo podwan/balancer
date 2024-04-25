b6a515548d58ebe84258f517b245f203f2c11e6b
inffydwan <7534571+inffydwan@user.noreply.gitee.com>
2024-04-25 23:32:49 +0800

    update rfBat pcb

--- firmware/devLog.md
+++ firmware/devLog.md
@@ -7,3 +7,9 @@
 关键字override提示函数覆写其父类的一个虚函数，编译器会检测函数签名是否一致
 
 当有个函数不想被override，则可用final修饰
+
+# 2024/4/25
+
+蓝牙BLE主从机通信参考
+
+[ESP32 BLE Server and Client (Bluetooth Low Energy) | Random Nerd Tutorials](https://randomnerdtutorials.com/esp32-ble-server-client/#ESP32-BLE-Server)
--- firmware/espBat/balancer/balancer.ino
+++ firmware/espBat/balancer/balancer.ino
@@ -1,216 +1,158 @@
+#include "BLEDevice.h"
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
-/***************************************************************************************************/
-// #include <Arduino.h>
-#include <NimBLEDevice.h>
-#include <EEPROM.h>
 
-NimBLEAdvertisedDevice* advDevice;
-bool doConnect = false;
-uint32_t scanTime = 0; /** 0 = scan forever */
-bool scanning = false, connected = false;
+#define POWER_EN 6
+//Default Temperature is in Celsius
+//Comment the next line for Temperature in Fahrenheit
+#define temperatureCelsius
 
-uint8_t eeprom_MacFlag = 0;
-uint8_t MacFlag = 0;
-uint16_t led_cnt = 0;
-uint8_t led_flag = 0;
-uint16_t led_hz = 50;
+//BLE Server name (the other ESP32 name running the server sketch)
+#define bleServerName "BME280_ESP32"
 
-// NimBLEAddress Address("00:00:00:00:00:00");                      // 蓝牙手柄地址（每个手柄地址都不一样），需自行根据实际地址修改
-// NimBLEAddress Address("04:03:23:68:8b:b0");
-#define BLE_Name "controller"  // 修改手柄实际名称，代码会根据搜到的手柄名称进行保存其读取到的蓝牙MAC地址
-std::string BLE_MAC;
-NimBLEUUID ServiceUUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");  // 蓝牙手柄有数据输出的服务UUID
+/* UUID's of the service, characteristic that we want to read*/
+// BLE Service
+static BLEUUID bmeServiceUUID("91bad492-b950-4226-aa2b-4ede9fa42f59");
 
-/***************************************************************************************************/
+// BLE Characteristics
+#ifdef temperatureCelsius
+//Temperature Celsius Characteristic
+static BLEUUID temperatureCharacteristicUUID("cba1d466-344c-4be3-ab3f-189f80dd7518");
+#else
+//Temperature Fahrenheit Characteristic
+static BLEUUID temperatureCharacteristicUUID("f78ebbff-c8b7-4107-93de-889a6a06d408");
+#endif
 
-/***************************************************************************************************/
-#include "driver/uart.h"
+// Humidity Characteristic
+static BLEUUID humidityCharacteristicUUID("ca73b3ba-39f6-4ab3-91ae-186dc9577d99");
 
+//Flags stating if should begin connecting and if the connection is up
+static boolean doConnect = false;
+static boolean connected = false;
+
+//Address of the peripheral device. Address will be found during scanning...
+static BLEAddress* pServerAddress;
+
+//Characteristicd that we want to read
+static BLERemoteCharacteristic* temperatureCharacteristic;
+static BLERemoteCharacteristic* humidityCharacteristic;
+
+//Activate notify
+const uint8_t notificationOn[] = { 0x1, 0x0 };
+const uint8_t notificationOff[] = { 0x0, 0x0 };
+
+//Variables to store temperature and humidity
+char* temperatureChar;
+char* humidityChar;
+
+//Flags to check whether new temperature and humidity readings are available
+boolean newTemperature = false;
+boolean newHumidity = false;
 HardwareSerial mySerial(1);
-/***************************************************************************************************/
+//Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress) {
+  BLEClient* pClient = BLEDevice::createClient();
 
-/***************************************************************************************************/
-class ClientCallbacks : public NimBLEClientCallbacks {
-  void onConnect(NimBLEClient* pClient) {
-    mySerial.println("链接成功");
-    connected = true;
-    digitalWrite(8, 0);
-  };
+  // Connect to the remove BLE Server.
+  pClient->connect(pAddress);
+  mySerial.println(" - Connected to server");
 
-  void onDisconnect(NimBLEClient* pClient) {
-    mySerial.println("链接失败");
-    connected = false;
-  };
-};
-
-/* 定义一个类来处理接收到广告时的回调 */
-class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
-  void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
-
-    mySerial.print("Advertised Device found: ");
-    mySerial.println(advertisedDevice->toString().c_str());
-    // if (advertisedDevice->getAddress() == Address) {
-    //   NimBLEDevice::getScan()->stop();
-    //   advDevice = advertisedDevice;
-    //   doConnect = true;
-    // }
-    // if (eeprom_MacFlag == 0) {
-    //   if (advertisedDevice->getName() == BLE_Name) {
-
-    //     std::string myString = advertisedDevice->getAddress();
-    //     eeprom_MacFlag = 1;
-    //     for (int i = 0; i < 17; i++) {
-    //       EEPROM.write(i, myString[i]);
-    //     }
-    //     EEPROM.write(17, eeprom_MacFlag);  // 保存蓝牙游戏手柄MAC地址
-    //     EEPROM.commit();
-    //     mySerial.print("MAC save ok!");
-    //     led_hz = 500;
-
-    //     NimBLEDevice::getScan()->stop();
-    //     advDevice = advertisedDevice;
-    //     doConnect = true;
-    //   }
-    // }
-    // if (eeprom_MacFlag == 1) {                                // 读取保存标志
-    //   std::string myString = advertisedDevice->getAddress();  // 获取所以蓝牙设备的MAC地址
-    //   for (int i = 0; i < 17; i++) {                          //如果17位MAC字符都一样
-    //     if (myString[i] == BLE_MAC[i]) MacFlag += 1;
-    //   }
-    //   if (MacFlag == 17) {  // 停止扫描，并启动链接
-    //     mySerial.print("MAC check ok!");
-    //     NimBLEDevice::getScan()->stop();
-    //     advDevice = advertisedDevice;
-    //     doConnect = true;
-    //   }
-    //   MacFlag = 0;
-    // }
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(bmeServiceUUID);
+  if (pRemoteService == nullptr) {
+    mySerial.print("Failed to find our service UUID: ");
+    mySerial.println(bmeServiceUUID.toString().c_str());
+    return (false);
   }
-};
 
+  // Obtain a reference to the characteristics in the service of the remote BLE server.
+  temperatureCharacteristic = pRemoteService->getCharacteristic(temperatureCharacteristicUUID);
+  humidityCharacteristic = pRemoteService->getCharacteristic(humidityCharacteristicUUID);
 
-/** Notification / Indication receiving handler callback */
-void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  uart_write_bytes(UART_NUM_1, (char*)pData, length);
-  mySerial.println("ShouBing data");
-}
-
-/** 回调处理最后一次扫描的结果或重新启动 */
-void scanEndedCB(NimBLEScanResults results) {
-  scanning = false;  //扫描状态 = 假
-}
-
-void charaSubscribeNotification(NimBLERemoteCharacteristic* pChara)  // 订阅通知函数
-{
-  if (pChara->canNotify()) {
-    mySerial.println("canNotify");
-    if (pChara->subscribe(true, notifyCB, true))  // 订阅通知
-    {
-      mySerial.println("set NotifyCb");
-    }
+  if (temperatureCharacteristic == nullptr || humidityCharacteristic == nullptr) {
+    mySerial.print("Failed to find our characteristic UUID");
+    return false;
   }
-}
+  mySerial.println(" - Found our characteristics");
 
-bool afterConnect(NimBLEClient* pClient) {
-  for (auto pService : *pClient->getServices(true)) {
-    auto sUuid = pService->getUUID();  // 逐一获取服务中的UUID
-
-    if (!sUuid.equals(ServiceUUID))  // 判断是否有所需的目标UUID
-    {
-      continue;  // 没有，则直接跳出函数体
-    }
-    for (auto pChara : *pService->getCharacteristics(true))  // 遍历服务Characteristics
-    {
-      charaSubscribeNotification(pChara);  // 判读服务特征是否有Notify属性，并订阅
-    }
-  }
+  //Assign callback functions for the Characteristics
+  temperatureCharacteristic->registerForNotify(temperatureNotifyCallback);
+  humidityCharacteristic->registerForNotify(humidityNotifyCallback);
   return true;
 }
 
-static ClientCallbacks clientCB;
-/** 处理客户端供应和连接/接口与服务器 */
-bool connectToServer(NimBLEAdvertisedDevice* advDevice) {
-  NimBLEClient* pClient = nullptr;
-  /** 创建一个回调类的全局实例供所有客户端使用 */
-
-  /** 检查我们是否有一个客户端，我们应该首先重用 **/
-  if (NimBLEDevice::getClientListSize()) {
-    pClient = NimBLEDevice::getClientByPeerAddress(advDevice->getAddress());
-    if (pClient) {
-      pClient->connect();
+//Callback function that gets called, when another device's advertisement has been received
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    if (advertisedDevice.getName() == bleServerName) {                 //Check if the name of the advertiser matches
+      advertisedDevice.getScan()->stop();                              //Scan can be stopped, we found what we are looking for
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress());  //Address of advertiser is the one we need
+      doConnect = true;                                                //Set indicator, stating that we are ready to connect
+      mySerial.println("Device found. Connecting!");
     }
   }
+};
 
-  if (!pClient) {
-    pClient = NimBLEDevice::createClient();
-    mySerial.println("New client created");
-    pClient->setClientCallbacks(&clientCB, false);
-    pClient->setConnectTimeout(5);
-    pClient->connect(advDevice, false);
-  }
-
-  mySerial.print("Connected to:");
-  mySerial.println(pClient->getPeerAddress().toString().c_str());
-
-  bool result = afterConnect(pClient);
-  if (!result) {
-    return result;
-  }
-  return true;
+//When the BLE Server sends a new temperature reading with the notify property
+static void temperatureNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
+                                      uint8_t* pData, size_t length, bool isNotify) {
+  //store temperature value
+  temperatureChar = (char*)pData;
+  newTemperature = true;
 }
 
-void startScan() {
-  scanning = true;
-  auto pScan = NimBLEDevice::getScan();
-  pScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
-  pScan->setInterval(45);
-  pScan->setWindow(15);
-  pScan->start(scanTime, scanEndedCB);
+//When the BLE Server sends a new humidity reading with the notify property
+static void humidityNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
+                                   uint8_t* pData, size_t length, bool isNotify) {
+  //store humidity value
+  humidityChar = (char*)pData;
+  newHumidity = true;
+  mySerial.print(newHumidity);
 }
-/***************************************************************************************************/
 
 
-/***************************************************************************************************/
 
 void setup() {
+  pinMode(POWER_EN, OUTPUT);
+
+
   mySerial.begin(115200, SERIAL_8N1, 20, 21);
-
-  Serial.println("Starting Arduino BLE Client application...");
-
   //Init BLE device
-  BLEDevice::init("");
+  // BLEDevice::init("");
+  // BLEScan* pBLEScan = BLEDevice::getScan();
+  // pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  // pBLEScan->setActiveScan(true);
+  // pBLEScan->start(30);
 
-  // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // have detected a new device.  Specify that we want active scanning and start the
-  // scan to run for 30 seconds.
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(30);
-  mySerial.println("set done\n");
+  mySerial.println("mcu init done\n");
 }
 
-uint8_t key_cnt = 0, Erase_flag = 0;
-
 void loop() {
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  // if (doConnect == true) {
+  //   if (connectToServer(*pServerAddress)) {
+  //     mySerial.println("We are now connected to the BLE Server.");
+  //     //Activate the Notify property of each Characteristic
+  //     temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+  //     humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+  //     connected = true;
+  //   } else {
+  //     mySerial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
+  //   }
+  //   doConnect = false;
+  // }
 
-  if (doConnect == true) {
-    if (connectToServer(*pServerAddress)) {
-      Serial.println("We are now connected to the BLE Server.");
-      //Activate the Notify property of each Characteristic
-      temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
-      humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
-      connected = true;
-    } else {
-      Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
-    }
-    doConnect = false;
-  }
-  //if new temperature readings are available, print in the OLED
-  if (newTemperature && newHumidity){
-    newTemperature = false;
-    newHumidity = false;
-    printReadings();
-  }
-  delay(1000); // Delay a second between loops.
+  // delay(1000); // Delay a second between loops.
+
+  digitalWrite(POWER_EN, HIGH);  //
+  mySerial.println("high\n");
+  delay(1000);  // Delay a second between loops.
+
+  digitalWrite(POWER_EN, LOW);  //
+  mySerial.println("low\n");
+  delay(1000);  // Delay a second between loops.
 }
