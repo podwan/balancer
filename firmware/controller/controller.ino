3cd2379384046215dcf2be8b05686406d5c0b11b
inffydwan <7534571+inffydwan@user.noreply.gitee.com>
2024-06-12 00:36:36 +0800

    udpate controller pcb

--- firmware/controller/controller.ino
+++ firmware/controller/controller.ino
@@ -19,25 +19,24 @@
 #define RU_SWITCH 0
 #define LU_SWTICH 6
 #define RGB 7
+#define LOW_POWER_VOLTAGE 3.3f
 
 #define RGB_COUNT 1
 
 #define CHANNEL 0
 
-#define POWER_LOW (3300 * 0.6) //(bat) mv * 0.6
-// #define POWER_LOW 2500  //(bat) mv * 0.6
 #define L_Y 4
 #define L_X 5
 #define R_Y 1
 #define R_X 2
 u8 m_color[7][3] = {
-    {255, 0, 0},
-    {0, 255, 0},
-    {0, 0, 255},
-    {255, 255, 0},
-    {255, 0, 255},
-    {0, 255, 255},
-    {0, 0, 0},
+  { 255, 0, 0 },
+  { 0, 255, 0 },
+  { 0, 0, 255 },
+  { 255, 255, 0 },
+  { 255, 0, 255 },
+  { 0, 255, 255 },
+  { 0, 0, 0 },
 };
 int delayval = 100;
 
@@ -50,40 +49,33 @@ hw_timer_t *timer = NULL;
 char lyValue, lxValue, ryValue, rxValue;
 // Joystick joystickL(4, 5, 13);
 extern unsigned char BLEBUF[];
-static void IRAM_ATTR Timer0_CallBack(void)
-{
+static void IRAM_ATTR Timer0_CallBack(void) {
   static int _1000msCnt, _20msCnt, _10msCnt, _500msCnt, _100msCnt;
   _1ms = 1;
-  if (++_1000msCnt >= 1000)
-  {
+  if (++_1000msCnt >= 1000) {
     _1000msCnt = 0;
     _1000ms = 1;
   }
-  if (++_500msCnt >= 500)
-  {
+  if (++_500msCnt >= 500) {
     _500msCnt = 0;
     _500ms = 1;
   }
-  if (++_20msCnt >= 20)
-  {
+  if (++_20msCnt >= 20) {
     _20msCnt = 0;
     _20ms = 1;
   }
 
-  if (++_10msCnt >= 10)
-  {
+  if (++_10msCnt >= 10) {
     _10msCnt = 0;
     _10ms = 1;
   }
 
-  if (++_100msCnt >= 100)
-  {
+  if (++_100msCnt >= 100) {
     _100msCnt = 0;
     _100ms = 1;
   }
 }
-void setup()
-{
+void setup() {
   // Start serial communication
   serial1.begin(115200, SERIAL_8N1, 20, 21);
 
@@ -92,8 +84,8 @@ void setup()
 
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, Timer0_CallBack, true);
-  timerAlarmWrite(timer, 1000, true); // 单位us,定时模式,10ms
-  timerAlarmEnable(timer);            // 启动定时器
+  timerAlarmWrite(timer, 1000, true);  // 单位us,定时模式,10ms
+  timerAlarmEnable(timer);             // 启动定时器
 
   strip.begin();
   strip.setBrightness(10);
@@ -110,17 +102,14 @@ void setup()
   serial1.println("MCU init done\n");
 }
 
-void loop()
-{
+void loop() {
 
-  if (_500ms)
-  {
+  if (_500ms) {
     _500ms = 0;
     blePolling();
   }
 
-  if (_100ms)
-  {
+  if (_100ms) {
     _100ms = 0;
     int rawValue = analogRead(L_Y);
     lyValue = map(rawValue, 348, 4095, 0, 100);
@@ -130,96 +119,79 @@ void loop()
     ryValue = map(rawValue, 348, 4095, 0, 100);
   }
 
-  if (powerOff)
-  {
+  if (powerOff) {
     digitalWrite(POWER_EN, LOW);
     strip.setLedColorData(0, m_color[6][0], m_color[6][1], m_color[6][2]);
     strip.show();
     delay(delayval);
-  }
-  else
-  {
-    digitalWrite(POWER_EN, HIGH); //
-    if (!powerLow)
-    {
+  } else {
+    digitalWrite(POWER_EN, HIGH);  //
+    if (!powerLow) {
 
-      if (_10ms)
-      {
+      if (_10ms) {
         _10ms = 0;
 
         static int j = 0;
         static int i = 0;
-        if (i < RGB_COUNT)
-        {
-          if (j < 255)
-          {
+        if (i < RGB_COUNT) {
+          if (j < 255) {
             j += 2;
             strip.setLedColorData(i, strip.Wheel((i * 256 / RGB_COUNT + j) & 255));
-          }
-          else
-          {
+          } else {
             j = 0;
             i++;
           }
           strip.show();
-        }
-        else
-        {
+        } else {
           i = 0;
         }
       }
     }
   }
 
-  if (_1ms)
-  {
+  if (_1ms) {
     _1ms = 0;
     static int keyDownCnt;
-    if (!powerOff)
-    {
-      if (digitalRead(POWER_KEY) == 0)
-      {
-        if (++keyDownCnt >= 1000)
-        {
+    if (!powerOff) {
+      if (digitalRead(POWER_KEY) == 0) {
+        if (++keyDownCnt >= 1000) {
           powerOff = 1;
         }
-      }
-      else
-      {
+      } else {
         keyDownCnt = 0;
       }
     }
   }
 
-  if (_1000ms)
-  {
-    BLEBUF[0] = 0xA5;    // 包头
-    BLEBUF[1] = lxValue; //
-    BLEBUF[2] = lyValue; //
-    BLEBUF[3] = rxValue; //
-    BLEBUF[4] = ryValue; //
+  if (_1000ms) {
+    BLEBUF[0] = 0xA5;     // 包头
+    BLEBUF[1] = lxValue;  //
+    BLEBUF[2] = lyValue;  //
+    BLEBUF[3] = rxValue;  //
+    BLEBUF[4] = ryValue;  //
 
-    BLEBUF[5] = 0x5A; // 包尾
+    BLEBUF[5] = 0x5A;  // 包尾
 
     // serial1.printf("%03d%03d%03d\n",lyValue, rxValue,ryValue);
     serial1.printf("%03d,%03d,%03d,%03d\n", BLEBUF[1], BLEBUF[2], BLEBUF[3], BLEBUF[4]);
 
     _1000ms = 0;
-    if (!powerOff)
-    {
-      uint adcValue, voltage;
-      adcValue = analogRead(BAT_FB);
-      voltage = analogReadMilliVolts(BAT_FB);
-      // serial1.printf("adcValue: %d, voltage: %d, batLow:%f\n", adcValue, voltage, POWER_LOW);
-      if (voltage < POWER_LOW)
-      {
-        powerLow = 1;
-        digitalWrite(POWER_EN, LOW);
-        strip.setLedColorData(0, m_color[0][0], m_color[0][1], m_color[0][2]);
-        strip.show();
-      }
-      else
-        powerLow = 0;
-    }
+    // if (!powerOff)
+    // {
+    float voltage;
+    //   adcValue = analogRead(BAT_FB);
+    voltage = analogReadMilliVolts(BAT_FB) / 1000.0f / 0.6f;
+    serial1.printf("voltage: %.1f\n", voltage);
+    
+    //   if (voltage < POWER_LOW)
+    //   {
+    //     powerLow = 1;
+    //     digitalWrite(POWER_EN, LOW);
+    //     strip.setLedColorData(0, m_color[0][0], m_color[0][1], m_color[0][2]);
+    //     strip.show();
+    //   }
+    //   else
+    //     powerLow = 0;
+    // }
   }
 }
